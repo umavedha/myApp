4a5eb2783658f5a27c4ab9ffefcd7e074734e3c2
Uma V <uma.v@dreamtechnology.ai>
2022-09-20 16:33:14 +0530

    another way of sharing data

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,12 +1,20 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-
+interface User {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent implements OnInit {
-  @Input() users: any;
+  @Input() users: User[] = [];
+  @Input() userData: any;
+  @Input() userIndex: number = 0;
   @Output() removeUserData = new EventEmitter();
 
   constructor() {}
@@ -14,7 +22,7 @@ export class UserDetailsComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.users);
   }
-  removeUser(i: number) {
-    this.removeUserData.emit(i);
+  removeUser() {
+    this.removeUserData.emit(this.userIndex);
   }
 }
