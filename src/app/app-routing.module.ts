0d968aa45c007331acf7e1679bfd48720734dc04
Uma V <uma.v@dreamtechnology.ai>
2022-09-29 16:34:00 +0530

    implemented a authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutuscomponentComponent } from './aboutuscomponent/aboutuscomponent.component';
+import { AuthGuard } from './auth.guard';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
 import { MainareacomponentComponent } from './mainareacomponent/mainareacomponent.component';
 import { OurgoalComponent } from './ourgoal/ourgoal.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -44,22 +46,29 @@ const routes: Routes = [
   //   component: PageNotFoundComponent,
   // },
 
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full',
+  // },
   {
-    path: '',
-    redirectTo: 'userlist',
-    pathMatch: 'full',
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: 'userlist',
     component: UsersListComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'userlist/add-user',
     component: FormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'userlist/edit-user/:userId',
     component: FormComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
